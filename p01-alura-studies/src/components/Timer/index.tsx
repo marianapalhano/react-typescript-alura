1830929d3f0c95545044a310ebfea8c8965fe1ba
Mariana <marianaufc@gmail.com>
2022-06-28 18:16:52 -0300

    feat(p01): gets time from selected task

--- p01-alura-studies/src/components/Timer/index.tsx
+++ p01-alura-studies/src/components/Timer/index.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
+import { timeToSeconds } from "../../common/utils/time";
+import { ITask } from "../../types/task";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./Timer.module.scss";
 
-export default function Timer() {
+interface Props {
+    selected: ITask | undefined
+}
+
+export default function Timer({ selected }: Props) {
+    const [time, setTime] = useState<number>();
+    useEffect(() => {
+        if (selected?.time) {
+            setTime(timeToSeconds(selected.time));
+        }
+    }, [selected])
+    
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+            Time: {time}
             <div className={style.relogioWrapper}>
                 <Clock />
             </div>
